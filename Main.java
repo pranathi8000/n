5a40f3ebf11880ab75cebf5a1b8b27e58114bc84
pranathi800 <ppranathi2003@gmail.com>
2025-06-11 07:45:11 +0530

    main

---
+++ Main.java
@@ -0,0 +1,11 @@
+package n;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = -5; // Change this number to test
+
+        String result = NumberChecker.check(number);
+        System.out.println("The number is " + result);
+    }
+}
+
